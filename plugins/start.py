229e6ca0976e616c9dff62ed8b5ce54435ec1374
RoninAnimeBots <nitinkumardhundhara@gmail.com>
2025-08-12 17:33:47 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -100,7 +100,7 @@ async def start_command(client: Client, message: Message):
             caption = (CUSTOM_CAPTION.format(previouscaption="" if not msg.caption else msg.caption.html, 
                                              filename=msg.document.file_name) if bool(CUSTOM_CAPTION) and bool(msg.document)
                        else ("" if not msg.caption else msg.caption.html))
-            reply_markup = msg.reply_markup if DISABLE_CHANNEL_BUTTON else None
+            reply_markup = msg.reply_markup if not DISABLE_CHANNEL_BUTTON else None
             try:
                 copied_msg = await msg.copy(
                     chat_id=message.from_user.id,
