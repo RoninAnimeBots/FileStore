a09b0ac41602cb5d393d2baa0bc884dd7748431a
RoninAnimeBots <nitinkumardhundhara@gmail.com>
2025-08-12 15:30:43 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -22,8 +22,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             text=HELP_TXT.format(first=query.from_user.first_name),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
-                 InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data='close')]
+                [InlineKeyboardButton("ᴄʟᴏꜱᴇ ✘", callback_data='close')]
             ])
         )
 
@@ -32,8 +31,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             text=ABOUT_TXT.format(first=query.from_user.first_name),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
-                 InlineKeyboardButton('ᴄʟᴏꜱᴇ', callback_data='close')]
+                [InlineKeyboardButton('ᴄʟᴏꜱᴇ ✘', callback_data='close')]
             ])
         )
 
